245c1df816a6f4e0d55b9a346535a0ffe45a91ec
javitocor <56809963+javitocor@users.noreply.github.com>
2020-04-09 11:35:57 +0200

    Created program to get the cookis, http code, headers and body of any search in Bing

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,22 @@
 require 'rest-client'
+
+def search(input)
+    param = input.split(" ").join("+")
+    @var = RestClient.get("https://www.bing.com/search?q=#{param}")
+    puts "The cookies on this site are: #{@var.cookies}"
+    puts "The Get code is: #{@var.code}" 
+end
+
+puts "Search Power"
+puts "Powered by Bing"
+sleep(1)
+puts "Add the keywords of your search in Bing: "
+input = gets.chomp
+puts search(input)
+puts "Do you want to get the headers and the body? (y/n)"
+input2 = gets.chomp
+if input2 == "y"
+     puts "The headers are: #{@var.headers}"
+     sleep (2)
+     puts "The body of your search is : #{@var.body}"
+end
